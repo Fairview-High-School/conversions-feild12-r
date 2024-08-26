c530f3192f13c81629dca235de0cfb2e133a965e
feild12-r <h2@spaghettiverse.com>
2024-08-26 18:15:33 +0000

    7, 8 ,9 ,10

--- Conversions.java
+++ Conversions.java
@@ -17,6 +17,10 @@ public class Conversions
         System.out.println("4. Meters to Feet");
         System.out.println("5. Ounces to Milliliters");
         System.out.println("6. Milliliters to Ounces");
+        System.out.println("7. Feet to Inches");
+        System.out.println("8. Inches to Feet");
+        System.out.println("9. Feet to Yards");
+        System.out.println("10. Yards to Feet");
         //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
         //You can add even more if you would like.
 
@@ -72,6 +76,38 @@ public class Conversions
             keyboard.nextLine();
             double ounces = milliters/29.574;
             System.out.println(milliters + " milliters is " + ounces + " ounces");
+        }
+        if (selection == 7)
+        {
+            System.out.println("Enter Feet: ");
+            double feet = keyboard.nextDouble();
+            keyboard.nextLine();
+            double inches = feet*12;
+            System.out.println(feet + " feet is " + inches + " inches");
+        }
+        if (selection == 8)
+        {
+            System.out.println("Enter Inches: ");
+            double inches = keyboard.nextDouble();
+            keyboard.nextLine();
+            double feet = inches/12;
+            System.out.println(inches + " inches is " + feet + " feet");
+        }
+        if (selection == 9)
+        {
+            System.out.println("Enter Feet: ");
+            double feet = keyboard.nextDouble();
+            keyboard.nextLine();
+            double yards = feet/12;
+            System.out.println(feet + " feet is " + yards + " yards");
+        }
+        if (selection == 10)
+        {
+            System.out.println("Enter Yards: ");
+            double yards = keyboard.nextDouble();
+            keyboard.nextLine();
+            double feet = yards/12;
+            System.out.println(yards + " yards is " + feet + " feet");
         }
     }
 }
